167ff0dc3201f3fa8ba1777a228ea9c0228f4c63
Dan Menard <dan.menard@shopify.com>
2019-04-03 10:15:49 -0400

    Hah, forgot to scope my JS. Fixed.

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,12 @@
-function getToken() {
-  fetch(new Request('/githubToken'))
-    .then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
+(function() {
+  function getToken() {
+    fetch(new Request('/githubToken'))
+      .then(function(response) {
+        response.json().then(function(data) {
+          console.log(data);
+        });
       });
-    });
-}
+  }
 
-getToken();
+  getToken();
+})();
