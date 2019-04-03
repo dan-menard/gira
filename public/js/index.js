d29befaeae27273ec6ea23aa2b8ba7b4a2e5b982
Dan Menard <dan.menard@shopify.com>
2019-04-03 10:51:11 -0400

    Fetch org projects by owner/repo.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,16 @@
 (function() {
-  function getToken() {
-    fetch(new Request('/githubToken'))
+  const githubApi = 'https://api.github.com/';
+  let token = '';
+
+  function getProjects() {
+    const orgSlashRepo = document.querySelector('#projectLocation').value;
+
+    fetch(githubApi + `repos/${orgSlashRepo}/projects`, {
+      headers: {
+        'Accept': 'application/vnd.github.inertia-preview+json',
+        'Authorization': `token ${token}`,
+      }
+    })
       .then(function(response) {
         response.json().then(function(data) {
           console.log(data);
@@ -8,5 +18,21 @@
       });
   }
 
-  getToken();
+  function getToken() {
+    fetch(new Request('/githubToken'))
+      .then(function(response) {
+        response.json().then(function(data) {
+          token = data.token;
+        });
+      });
+  }
+
+  function setEventHandlers() {
+    document.querySelector('#loadProjectsButton').addEventListener('click', getProjects);
+  }
+
+  window.onload = function() {
+    setEventHandlers();
+    getToken();
+  };
 })();
