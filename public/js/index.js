9e21a8a6f433f4e1db6380262c52920f8c37b91d
Dan Menard <dan.menard@shopify.com>
2019-04-03 10:09:57 -0400

    Read github token and fetch it from the view.

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+function getToken() {
+  fetch(new Request('/githubToken'))
+    .then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+}
+
+getToken();
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 
+app.use(express.static('public'))
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/githubToken', function(req, res) {
+  fs.readFile(path.join(__dirname + '/.env'), function(err, data) {
+    if (err) {
+      next(err);
+    }
+
+    const tokenWithNewline = data.toString().split('=')[1];
+    const token = tokenWithNewline.split('\n')[0];
+
+    res.json({token});
+  });
 });
 
 app.listen(8080);
